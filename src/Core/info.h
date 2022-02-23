a8533b732530623af57f420fc8b9695bfe6daa24
GrimFlash <65523164+GrimFlash@users.noreply.github.com>
2022-02-22 21:04:41 -0500

    Remove donations

--- src/Core/info.h
+++ src/Core/info.h
@@ -9,7 +9,5 @@
 #define MOD_LINK_FORUM L"https://steamcommunity.com/app/702890/discussions/0/1694923613855843759/"
 #define MOD_LINK_GITHUB L"https://github.com/kovidomi/BBTAG-Improvement-Mod"
 
-#define MOD_LINK_DONATE L"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=C8EDR78DJ8PU2"
-#define MOD_LINK_DONATORS L"https://github.com/kovidomi/BBTAG-Improvement-Mod/raw/master/resource/donators.txt"
 #define MOD_LINK_PALETTES_ARCHIVE L"https://github.com/kovidomi/BBTAG-Improvement-Mod/raw/master/resource/palettes.tar.gz"
 #define MOD_LINK_PALETTES_HASH L"https://github.com/kovidomi/BBTAG-Improvement-Mod/raw/master/resource/palettes_hash.txt"
