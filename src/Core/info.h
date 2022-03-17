ab7337d97e57b686e8d0bbaa82dfdef7065af2f0
GrimFlash <65523164+GrimFlash@users.noreply.github.com>
2022-03-16 20:12:30 -0400

    Update links

--- src/Core/info.h
+++ src/Core/info.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#define MOD_VERSION_NUM	"v1.09"
-#define MOD_VERSION_NUM_INTERNAL 109
+#define MOD_VERSION_NUM	"v1.10"
+#define MOD_VERSION_NUM_INTERNAL 110
 #define MOD_WINDOW_TITLE "BBTAG Improvement Mod"
 
 //Links
 #define MOD_LINK_DISCORD L"https://discord.gg/j2mCX9s"
-#define MOD_LINK_FORUM L"https://steamcommunity.com/app/702890/discussions/0/1694923613855843759/"
-#define MOD_LINK_GITHUB L"https://github.com/kovidomi/BBTAG-Improvement-Mod"
+#define MOD_LINK_FORUM L"https://steamcommunity.com/app/702890/discussions/0/3185740024678823087/"
+#define MOD_LINK_GITHUB L"https://github.com/GrimFlash/BBTAG-Improvement-Mod"
 
-#define MOD_LINK_PALETTES_ARCHIVE L"https://github.com/kovidomi/BBTAG-Improvement-Mod/raw/master/resource/palettes.tar.gz"
-#define MOD_LINK_PALETTES_HASH L"https://github.com/kovidomi/BBTAG-Improvement-Mod/raw/master/resource/palettes_hash.txt"
+#define MOD_LINK_PALETTES_ARCHIVE L"https://github.com/GrimFlash/BBTAG-Improvement-Mod/raw/master/resource/palettes.tar.gz"
+#define MOD_LINK_PALETTES_HASH L"https://github.com/GrimFlash/BBTAG-Improvement-Mod/raw/master/resource/palettes_hash.txt"
